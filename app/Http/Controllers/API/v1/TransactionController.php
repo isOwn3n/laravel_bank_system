c950d784c067836936c07886c07866f530be37f2
Ownen <mr.joker.mr.joke@gmail.com>
2024-10-31 00:40:39 +0330

    complete get_three_last_users controller (handle it by redis).

--- app/Http/Controllers/API/v1/TransactionController.php
+++ app/Http/Controllers/API/v1/TransactionController.php
@@ -19,7 +19,7 @@ class TransactionController extends Controller
 
     public function testRedis()
     {
-        return response()->json($this->read_from_redis_by_user_id(1));
+        return response()->json($this->read_all_from_redis());
     }
 
     /**
@@ -39,20 +39,40 @@ class TransactionController extends Controller
         return $result;
     }
 
-    public function get_three_last_users(Request $request)
+    /**
+     * @return JsonResponse
+     */
+    public function get_three_last_users(): JsonResponse
     {
         $cache_transaction = $this->read_all_from_redis();
-        $user_ids = [];
-        foreach ($cache_transaction as $index => $value) {
-            $parts = explode(':', $value);
-            array_push($user_ids, (int) $parts[1]);
+        $transactions = [];
+        foreach ($cache_transaction as $transaction) {
+            // TODO: Clean this code.
+            $redisKey = str_replace('laravel_database_', '', $transaction);
+            $transaction_data = $this->get_from_redis($redisKey);
+            $amount = json_decode($transaction_data, true)['amount'] ?? 0;
+            $user_id = explode(':', $redisKey)[1];
+
+            if (!isset($transactions[$user_id])) {
+                $transactions[$user_id] = [];
+            }
+            $transactions[$user_id][] = $amount;
         }
-        return response()->json($user_ids);
+        $totalAmounts = [];
+        foreach ($transactions as $userId => $amounts) {
+            $totalAmounts[$userId] = array_sum($amounts);
+        }
+        arsort($totalAmounts);
+        $top3Users = array_slice($totalAmounts, 0, 3, true);
+
+        $transactions_result = $this->service->reposotiry->get_last_ten_rows($top3Users);
+        return response()->json($transactions_result);
     }
 
     /**
      * Store a newly created resource in storage.
      * @param Request $request
+     * @return JsonResponse
      */
 
     // TODO: Remove it and write a query (One with SQL and the other one with ORM)
