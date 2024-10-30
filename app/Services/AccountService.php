29a58b6e7f8c507c172fb372505e55d938eb7570
Ownen <mr.joker.mr.joke@gmail.com>
2024-10-31 00:45:36 +0330

    write php docstring and add fee to function as param

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -10,12 +10,24 @@ class AccountService
         public AccountRepository $repository,
     ) {}
 
-    public function update_balance(int $amount, int $card_number, int $user_id, bool $is_deposit = true): array
+    /**
+     * @param int $amount
+     * @param int $card_number
+     * @param int $user_id
+     * @param bool $is_deposit
+     * @param int $fee
+     * @return ?array
+     */
+    public function update_balance(int $amount, int $card_number, int $user_id, bool $is_deposit = true, int $fee = 0): ?array
     {
         if ($this->repository->is_account_able($card_number, $amount))
-            return $this->repository->update_balance($amount, $card_number, $user_id, $is_deposit);
+            return $this->repository->update_balance($amount, $card_number, $user_id, $is_deposit, $fee);
     }
 
+    /**
+     * @param int $card_number
+     * @return int
+     */
     public function get_total_transactions_of_day(int $card_number): int
     {
         return $this->repository->today_total_amount($card_number);
