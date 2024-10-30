c7d50ddba6f0d9c683fda69ca98e5f1ac91f14b5
Ownen <mr.joker.mr.joke@gmail.com>
2024-10-31 00:51:36 +0330

    write a new seeder to sync cards user_id and transactions user_id

---
+++ database/seeders/UserTransactionSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\Transaction;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $accounts = Account::all();
+
+        foreach ($accounts as $account) {
+            Transaction::where('card_id', $account->id)
+                ->update(['user_id' => $account->user_id]);
+        }
+    }
+}
