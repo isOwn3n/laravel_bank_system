9b040743e59fb68127d8710679d0ef68fb2bd431
Ownen <mr.joker.mr.joke@gmail.com>
2024-11-01 03:07:29 +0330

    changed name of user endpoint and move it to auth group as me. change rate limitter middleware from api to transaction.

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,21 @@ use App\Http\Controllers\API\v1\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 });
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::middleware(['throttle:api', 'is-transaction-limit-reached'])->group(function () {
+    Route::middleware(['throttle:transaction', 'is-transaction-limit-reached'])->group(function () {
         Route::post('/cash', [TransactionController::class, 'cash']);
         Route::post('/transfer', [TransactionController::class, 'transfer']);
     });
     Route::get('/balance', [TransactionController::class, 'getBalance']);
     Route::get('/top-users', [TransactionController::class, 'getThreeLastUsers']);
+
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
 });
